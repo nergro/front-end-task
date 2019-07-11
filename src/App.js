fab8b6144584beacc4a3e8c20c0e8fe2485596fa
Nerijus <nerius10@gmail.com>
2019-07-11 18:57:44 +0300

    Adding responsive contacts section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Welcome from './components/Welcome/Welcome';
 import Wireframe from './components/Wireframe/Wireframe';
+import Contacts from './components/Contacts/Contacts';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <Header />
         <Welcome />
         <Wireframe />
+        <Contacts />
       </div>
     );
   }
---
+++ src/components/Contacts/Contacts.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './Contacts.css';
+
+const contacts = () => {
+  return (
+    <div className='contacts'>
+      <div className='contacts-head'>
+        <h1>CONTACT</h1>
+        <hr />
+      </div>
+      <div className='contacts-content'>
+        <div className='contacts-content-left'>
+          <p>
+            Malesuada fames ac turpis egestas. Consequat ac felis donec et odio
+            pellentesque diam volutpat. Commodo odio aenean sed adipiscing.
+            Lobortis feugiat vivamus at augue eget. Mauris nunc congue nisi
+            vitae.
+          </p>
+          <p>
+            Nunc sed blandit libero volutpat sed. At risus viverra adipiscing at
+            in tellus integer. Risus quis varius quam quisque. Tortor consequat
+            id porta nibh venenatis cras sed felis eget.
+          </p>
+          <p>
+            Dignissim diam quis enim lobortis scelerisque fermentum dui
+            faucibus. Morbi tristique senectus et netus et malesuada. In hac
+            habitasse platea dictumst quisque sagittis.
+          </p>
+        </div>
+        <div className='contacts-content-right'>
+          <form className='message-form'>
+            <div className='form-control'>
+              <label for='name'>NAME</label>
+              <input type='text' name='name' autocomplete='off' />
+            </div>
+            <div className='form-control'>
+              <label for='email'>EMAIL</label>
+              <input type='email' name='email' autocomplete='off' />
+            </div>
+            <div className='form-control'>
+              <label for='message'>MESSAGE</label>
+              <textarea name='message' rows='10' />
+            </div>
+            <button type='submit' className='form-button'>
+              SEND
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default contacts;
