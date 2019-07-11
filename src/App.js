7282b6abb7bf0ffec5b418abdb182d54a12cf1d7
Nerijus <nerius10@gmail.com>
2019-07-11 13:25:42 +0300

    Adding mobile navbar + handling open/close logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 
-function App() {
-  return (
-    <div className='App'>
-      <Header />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Header />
+      </div>
+    );
+  }
 }
 
 export default App;
